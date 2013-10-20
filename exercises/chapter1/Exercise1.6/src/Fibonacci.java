aea02568579e82e95598c02ee0d42bc8ab3c799c
Hideki SHIRO <hideki@shiro.be>
2013-10-20 12:28:49 +0900

    練習問題1.6を追加

---
+++ exercises/chapter1/Exercise1.6/src/Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+
+	public static int MAX = 50;
+	static final String TITLE = "値が" + MAX + "未満のフィボナッチ数列";
+	
+	public static void main(String[] args) {
+		int lo = 1;
+		int hi = 1;
+		System.out.println(TITLE);
+		System.out.println(lo);
+		while (hi < 50) {
+			System.out.println(hi);
+			hi = lo + hi; // 新しいhi
+			lo = hi - lo; // 新しいloは(合計-古いlo1)、すなわち古いhi
+		}
+	}
+
+}
