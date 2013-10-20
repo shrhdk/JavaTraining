eb0b2c1232bf5eef574138face38905f1b40b62c
Hideki SHIRO <hideki@shiro.be>
2013-10-20 12:28:29 +0900

    Javaのコーディング規約に従って、名前付き定数の名前を大文字アンダースコア区切りに変更した。

--- exercises/chapter1/Exercise1.5/src/HelloWorld.java
+++ exercises/chapter1/Exercise1.5/src/HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
 
-	static final String message = "Hello, world";
+	static final String MESSAGE = "Hello, world";
 
 	public static void main(String[] args) {
-		System.out.println(message);
+		System.out.println(MESSAGE);
 	}
 
 }
