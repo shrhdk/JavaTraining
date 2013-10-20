57dec45a01a215ede82326f4b4b4d6f699d494ad
Hideki SHIRO <hideki@shiro.be>
2013-10-20 11:57:40 +0900

    練習問題1.2を追加

---
+++ exercises/chapter1/Exercise1.2/src/HelloWorldError.java
@@ -0,0 +1,8 @@
+public class HelloWorldError {
+
+	public static void main(String[] args) {
+		String helloWorldError = null;
+		System.out.println(helloWorldError.toString()); // NullPointerException is expected
+	}
+
+}
