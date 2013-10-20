e4d00edf4c0f106ebcc9bae2664ebb09681385dd
Hideki SHIRO <hideki@shiro.be>
2013-10-20 21:52:37 +0900

    練習問題1.10を追加

---
+++ exercises/chapter1/Exercise1.10/src/jpl/chapter1/exercise10/ImprovedFibonacci.java
@@ -0,0 +1,49 @@
+package jpl.chapter1.exercise10;
+
+public class ImprovedFibonacci {
+
+    static final int MAX_INDEX = 9;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static FibonacciNumber[] build(final int maxIndex) throws IllegalArgumentException {
+
+	if (maxIndex <= 0)
+	    throw new IllegalArgumentException("maxIndex needs to be 1 and over.");
+
+	int lo = 1;
+	int hi = 1;
+
+	ImprovedFibonacci.FibonacciNumber[] fibonacci = new ImprovedFibonacci.FibonacciNumber[maxIndex];
+
+	fibonacci[0] = new ImprovedFibonacci.FibonacciNumber(1);
+
+	if (2 <= maxIndex) {
+	    for (int i = 1; i < maxIndex; i++) {
+		fibonacci[i] = new ImprovedFibonacci.FibonacciNumber(hi);
+		hi = lo + hi;
+		lo = hi - lo;
+	    }
+	}
+
+	return fibonacci;
+    }
+
+    public static class FibonacciNumber{
+	int number;
+
+	public FibonacciNumber(final int number) {
+	    this.number = number;
+	}
+
+	public int getNumber() {
+	    return number;
+	}
+
+	public boolean isEven() {
+	    return number % 2 == 0;
+	}
+    }
+}
