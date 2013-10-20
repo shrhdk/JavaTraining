a8bc7a39d561c50363e1b9439639af89f140108e
Hideki SHIRO <hideki@shiro.be>
2013-10-20 21:51:09 +0900

    ・フィボナッチ数列生成関数をリネーム ・それに合わせてテスト関数をリネーム ・Fibonacci.buildの引数が負の場合をハンドリングできていないバグを修正 ・それに合わせたテストを追加

--- exercises/chapter1/Exercise1.9/src/jpl/chapter1/exercise9/Fibonacci.java
+++ exercises/chapter1/Exercise1.9/src/jpl/chapter1/exercise9/Fibonacci.java
@@ -2,37 +2,37 @@ package jpl.chapter1.exercise9;
 
 public class Fibonacci {
 
-	static final int MAX_INDEX = 9;
+    static final int MAX_INDEX = 9;
+
+    public static void main(String[] args) {
+	int[] fibonacci = build(MAX_INDEX);
+
+	String mark = "";
+	for (int i = 0; i < fibonacci.length; i++) {
+	    System.out.println(i + ": " + fibonacci[i] + mark);
+	}
+    }
+
+    public static int[] build(final int maxIndex) throws IllegalArgumentException {
+	if (maxIndex < 1) {
+	    throw new IllegalArgumentException("maxIndex needs to be 1 and over.");
+	}
+
+	int lo = 1;
+	int hi = 1;
+
+	int[] fibonacci = new int[maxIndex];
 	
-	public static void main(String[] args) {
-		int[] fibonacci = buildFibonacci(MAX_INDEX);
-		
-		String mark = "";
-		for(int i = 0; i < fibonacci.length; i++){
-			System.out.println(i + ": " + fibonacci[i] + mark);
-		}
+	fibonacci[0] = lo;
+
+	if (2 <= maxIndex) {
+	    for (int i = 1; i < maxIndex; i++) {
+		fibonacci[i] = hi;
+		hi = lo + hi;
+		lo = hi - lo;
+	    }
 	}
 
-	public static int[] buildFibonacci(final int maxIndex) throws IllegalArgumentException {
-		int[] fibonacci = new int[maxIndex];
-
-		if (maxIndex < 1) {
-			throw new IllegalArgumentException("maxIndex needs to be 1 and over.");
-		}
-
-		int lo = 1;
-		int hi = 1;
-
-		fibonacci[0] = lo;
-
-		if (2 <= maxIndex) {
-			for (int i = 1; i < maxIndex; i++) {
-				fibonacci[i] = hi;
-				hi = lo + hi;
-				lo = hi - lo;
-			}
-		}
-
-		return fibonacci;
-	}
+	return fibonacci;
+    }
 }
