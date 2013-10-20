94a6da2c92407eeedd0f1e5ec6cffd25c0643552
Hideki SHIRO <hideki@shiro.be>
2013-10-20 21:59:53 +0900

    練習問題1.11を追加

---
+++ exercises/chapter1/Exercise1.11/src/jpl/chapter1/exercise11/StringsDemo.java
@@ -0,0 +1,12 @@
+package jpl.chapter1.exercise11;
+
+public class StringsDemo {
+
+    public static void main(String[] args) {
+	String myName = "Takeshi";
+	
+	myName = myName + " Goda";
+	System.out.println("Name = " + myName);
+    }
+
+}
