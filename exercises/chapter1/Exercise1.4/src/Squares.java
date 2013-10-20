5c2061d6fd9b9370021ccacef10b4ea29f181de2
Hideki SHIRO <hideki@shiro.be>
2013-10-20 12:12:16 +0900

    練習問題1.4を追加

---
+++ exercises/chapter1/Exercise1.4/src/Squares.java
@@ -0,0 +1,10 @@
+public class Squares {
+
+	public static void main(String[] args) {
+		System.out.println("10–¢–ž‚Ì•½•û•\");
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i + " * " + i + " = " + i * i);
+		}
+	}
+
+}
