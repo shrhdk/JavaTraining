478a6d2333d0f3ec367fc693bf793628972d7dbb
Hideki SHIRO <hideki@shiro.be>
2013-10-27 23:45:52 +0900

    練習問題2.7を追加

---
+++ exercises/chapter2/Exercise2.7/src/jpl/chapter2/exercise7/Vehicle.java
@@ -0,0 +1,28 @@
+package jpl.chapter2.exercise7;
+
+public class Vehicle {
+
+    private static int nextId = 0;
+    private final int id;
+    public double velocity;
+    public double direction;
+    public String owner;
+
+    public static void resetId() {
+	Vehicle.nextId = 0;
+    }
+
+    public int getId() {
+	return this.id;
+    }
+
+    public Vehicle() {
+	this.id = Vehicle.nextId++;
+    }
+
+    public Vehicle(String owner) {
+	this();
+	this.owner = owner;
+    }
+
+}
---
+++ exercises/chapter2/Exercise2.7/test/src/jpl/chapter2/exercise7/VehicleTest.java
@@ -0,0 +1,38 @@
+package jpl.chapter2.exercise7;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class VehicleTest {
+
+    @Before
+    public void setup() {
+	Vehicle.resetId();
+    }
+
+    @Test
+    public void testVehicle() {
+	Vehicle vehicle1 = new Vehicle();
+	Vehicle vehicle2 = new Vehicle();
+	Vehicle vehicle3 = new Vehicle();
+
+	assertEquals(0, vehicle1.getId());
+	assertEquals(1, vehicle2.getId());
+	assertEquals(2, vehicle3.getId());
+    }
+
+    @Test
+    public void testVehicleWithOwner() {
+	Vehicle vehicle1 = new Vehicle("hoge");
+	Vehicle vehicle2 = new Vehicle("fuga");
+	Vehicle vehicle3 = new Vehicle("piyo");
+
+	assertEquals(0, vehicle1.getId());
+	assertEquals("hoge", vehicle1.owner);
+	assertEquals(1, vehicle2.getId());
+	assertEquals("fuga", vehicle2.owner);
+	assertEquals(2, vehicle3.getId());
+	assertEquals("piyo", vehicle3.owner);
+    }
+
+}
