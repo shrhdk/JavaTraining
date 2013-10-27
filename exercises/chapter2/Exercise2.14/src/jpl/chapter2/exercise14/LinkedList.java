a05524f4ad0060f6caf630eed1cdeb995bc9a0be
Hideki SHIRO <hideki@shiro.be>
2013-10-27 23:47:51 +0900

    練習問題2.14を追加

---
+++ exercises/chapter2/Exercise2.14/src/jpl/chapter2/exercise14/LinkedList.java
@@ -0,0 +1,52 @@
+package jpl.chapter2.exercise14;
+
+/**
+ * LinkedListクラスにprivateにすべきフィールドは見当たりません。
+ * value、nextともに外部から読み書きされても支障がありません。
+ * また、クラスの契約として含んでも後々に支障も出ないでしょう。
+ */
+public class LinkedList {
+    
+    private static final String stringForNullValue = "null";
+    public Object value = null;
+    public LinkedList next = null;
+
+    @Override
+    public String toString() {
+	String string;
+
+	if (this.value != null) {
+	    string = this.value.toString();
+	} else {
+	    string = stringForNullValue;
+	}
+
+	if (this.next != null) {
+	    string += " -> " + this.next.toString();
+	}
+	return string;
+    }
+
+    public LinkedList() {
+    }
+
+    public LinkedList(Object value) {
+	this.value = value;
+    }
+
+    public LinkedList(Object value, LinkedList next) {
+	this.value = value;
+	this.next = next;
+    }
+
+    public LinkedList(Object... objects) {
+	this(0, objects);
+    }
+
+    private LinkedList(int offset, Object... objects) {
+	this.value = objects[offset++];
+	if (offset < objects.length)
+	    this.next = new LinkedList(offset, objects);
+    }
+    
+}
