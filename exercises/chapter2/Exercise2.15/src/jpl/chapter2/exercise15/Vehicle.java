23f7619293e92a01af61c836e641510412aa9a2a
Hideki SHIRO <hideki@shiro.be>
2013-10-27 23:48:06 +0900

    練習問題2.15を追加

---
+++ exercises/chapter2/Exercise2.15/src/jpl/chapter2/exercise15/Vehicle.java
@@ -0,0 +1,73 @@
+package jpl.chapter2.exercise15;
+
+public class Vehicle {
+
+    private static int nextId = 0;
+    private final int id;
+    private double velocity;
+    private double direction;
+    private String owner;
+
+    public static int getMaximumId() throws IllegalStateException {
+	if (Vehicle.nextId == 0) {
+	    throw new IllegalStateException();
+	} else {
+	    return Vehicle.nextId - 1;
+	}
+    }
+
+    public static void resetId() {
+	Vehicle.nextId = 0;
+    }
+
+    public int getId() {
+	return this.id;
+    }
+
+    public double getVelocity() {
+	return this.velocity;
+    }
+
+    public void setVelocity(double velocity) {
+	this.velocity = velocity;
+    }
+
+    public void changeSpeed(double speed) {
+	setVelocity(speed);
+    }
+    
+    public void stop() {
+	setVelocity(0);
+    }
+    
+    public double getDirection() {
+	return this.direction;
+    }
+
+    public void setDirection(double direction) {
+	this.direction = direction;
+    }
+
+    public String getOwner() {
+	return this.owner;
+    }
+
+    public void setOwner(String owner) {
+	this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+	return String.format("%d (%s)", this.getId(), this.owner);
+    }
+
+    public Vehicle() {
+	this.id = Vehicle.nextId++;
+    }
+
+    public Vehicle(String owner) {
+	this();
+	this.setOwner(owner);
+    }
+
+}
