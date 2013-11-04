2c07f6b9a968db479b28a370fb955f3568d849bb
Hideki SHIRO <hideki@shiro.be>
2013-11-04 15:22:32 +0900

    練習問題3.2を追加

---
+++ exercises/chapter3/Exercise3.2/src/jpl/chapter3/exercise2/X.java
@@ -0,0 +1,22 @@
+package jpl.chapter3.exercise2;
+
+public class X {
+
+    protected int xMask = 0x00ff;
+    protected int fullMask;
+    
+    {
+	Observer.add("Init X field", xMask, 0, fullMask);
+    }
+    
+    public X() {
+	Observer.add("Call X constructor", xMask, 0, fullMask);
+	fullMask = xMask;
+	Observer.add("Exec X constructor", xMask, 0, fullMask);
+    }
+    
+    public int mask(int orig) {
+	return (orig & fullMask);
+    }
+
+}
---
+++ exercises/chapter3/Exercise3.2/src/jpl/chapter3/exercise2/Y.java
@@ -0,0 +1,16 @@
+package jpl.chapter3.exercise2;
+
+public class Y extends X {
+
+    protected int yMask = 0xff00;
+    
+    {
+	Observer.add("Init Y field", xMask, yMask, fullMask);
+    }
+    
+    public Y() {
+	fullMask |= yMask;
+	Observer.add("Exec Y constructor", xMask, yMask, fullMask);
+    }
+    
+}
