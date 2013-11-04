e5db791d25afae7b587c56bbbab516d507fbd140
Hideki SHIRO <hideki@shiro.be>
2013-11-04 15:23:10 +0900

    練習問題3.3を追加

---
+++ exercises/chapter3/Exercise3.3/src/jpl/chapter3/exercise3/X.java
@@ -0,0 +1,20 @@
+package jpl.chapter3.exercise3;
+
+public class X {
+
+    protected int fullMask = 0;
+    protected final int xMask = 0x00ff;
+
+    protected int getXMask() {
+	return xMask;
+    }
+
+    public X() {
+	System.out.printf("== Constructor of X ==%n");
+	System.out.printf("# fullMask => 0x%04x, X.xMask => 0x%04x, getXmask() => 0x%04x%n", fullMask, xMask, getXMask());
+	System.out.printf("> fullMask |= getXMask();%n");
+	fullMask |= getXMask();
+	System.out.printf("# fullMask => 0x%04x%n", fullMask);
+    }
+
+}
---
+++ exercises/chapter3/Exercise3.3/src/jpl/chapter3/exercise3/Y.java
@@ -0,0 +1,36 @@
+package jpl.chapter3.exercise3;
+
+public class Y extends X {
+
+    protected final int xMask = 0x000f;
+    protected final int yMask = 0xff00;
+
+    @Override
+    protected int getXMask() {
+	return xMask;
+    }
+
+    protected int getYMask() {
+	return yMask;
+    }
+
+    public Y() {
+	System.out.printf("== Constructor of Y ==%n");
+	System.out.printf("# fullMask => 0x%04x, Y.xMask => 0x%04x, getXmask() => 0x%04x%n", fullMask, xMask, getXMask());
+	System.out.printf("# fullMask => 0x%04x, Y.yMask => 0x%04x, getYmask() => 0x%04x%n", fullMask, yMask, getYMask());
+	System.out.printf("> fullMask |= getYMask();%n");
+	fullMask |= getYMask();
+	System.out.printf("# fullMask => 0x%04x", fullMask);
+    }
+
+    public static void main(String[] args) {
+	System.out.println("Exercise 3.3");
+	System.out.println("Answer: Get field via getter method instead of immediate access, and override that getter method in subclasses.");
+	System.out.println();
+	System.out.println("Example: Please note that difference of xMask and getXMask()");
+	System.out.printf("== main ==%n");
+	System.out.printf("> Y y = new Y()%n");
+	@SuppressWarnings("unused")
+	Y y = new Y();
+    }
+}
