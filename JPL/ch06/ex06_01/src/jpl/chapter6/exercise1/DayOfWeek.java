76c0398c7f24fce8cf92175a5a5820704b0982c8
Hideki SHIRO <hideki@shiro.be>
2013-12-03 01:39:18 +0900

    練習問題6.1を追加

---
+++ JPL/ch06/ex06_01/src/jpl/chapter6/exercise1/DayOfWeek.java
@@ -0,0 +1,5 @@
+package jpl.chapter6.exercise1;
+
+public enum DayOfWeek {
+	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ JPL/ch06/ex06_01/src/jpl/chapter6/exercise1/Signal.java
@@ -0,0 +1,5 @@
+package jpl.chapter6.exercise1;
+
+public enum Signal {
+	RED, GREEN, YELLOW
+}
