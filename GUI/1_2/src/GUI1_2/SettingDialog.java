d26a5192f3b16eea2834229315ec18f7f2f574c5
Hideki Shiro <hideki@shiro.be>
2014-01-08 03:41:02 +0900

    GUI課題1-2を追加

--- GUI/1_2/src/GUI1_2/SettingDialog.java
+++ GUI/1_2/src/GUI1_2/SettingDialog.java
@@ -3,38 +3,72 @@ package GUI1_2;
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.JComboBox;
+
 public class SettingDialog extends Dialog implements ActionListener {
-	
+
 	private static final long serialVersionUID = 1L;
 
+	// ComboBox Data
+	private static final Integer[] fontSize = { 50, 100, 150 };
+	private final String[] fontFamilies;
+	private static Color[] color = { Color.RED, Color.BLUE, Color.BLACK,
+			Color.WHITE, Color.GREEN, Color.GRAY, Color.YELLOW };
+
+	// UI Elements
+	private JComboBox fontSelector;
+	private JComboBox fontSizeSelector;
+	private JComboBox fontColorSelector;
+	private JComboBox backGroundColorSelector;
+
 	private Tokei tokei;
-	
+
 	public SettingDialog(Tokei tokei) {
 		super(tokei);
 		this.tokei = tokei;
+
+		// Close
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				setVisible(false);
+			}
+		});
 		
+		// Get font list
+		GraphicsEnvironment ge = GraphicsEnvironment
+				.getLocalGraphicsEnvironment();
+		fontFamilies = ge.getAvailableFontFamilyNames();
+
+		// Set up UI elements
+		fontSelector = new JComboBox(fontFamilies);
+		fontSizeSelector = new JComboBox(fontSize);
+		fontColorSelector = new JComboBox(color);
+		backGroundColorSelector = new JComboBox(color);
+
+		// OK button
+		Button okButton = new Button("OK");
+		okButton.addActionListener(this);
+
 		// Layout
-        setLayout(new FlowLayout());
-        setTitle("Tokei settings");
-        setSize(80, 80);
-        
-        // OK button
-        Button okButton = new Button("OK");
-        okButton.addActionListener(this);
-        add(okButton);
-        
-        // Font selector
-        
-        // Font size selector
-        
-        // Font color selector
-        
-        // Background color selector
-        
+		setLayout(new FlowLayout());
+		setTitle("Tokei settings");
+		setSize(300, 200);
+
+		// Add UI elements
+		add(fontSelector);
+		add(fontSizeSelector);
+		add(fontColorSelector);
+		add(backGroundColorSelector);
+		add(okButton);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
+		tokei.fontFamily = (String) fontSelector.getSelectedItem();
+		tokei.fontSize = (Integer) fontSizeSelector.getSelectedItem();
+		tokei.fontColor = (Color) fontColorSelector.getSelectedItem();
+		tokei.backGroundColor = (Color) backGroundColorSelector.getSelectedItem();
+		setVisible(false);
 	}
 }
--- GUI/1_2/src/GUI1_2/Tokei.java
+++ GUI/1_2/src/GUI1_2/Tokei.java
@@ -8,11 +8,24 @@ public class Tokei extends Frame implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 
+	private static final int MARGIN = 30;
+
+	// Buffer
+	Image imageBuffer;
+	Graphics graphicsBuffer;
+
+	// Settings
+	public String fontFamily = null;
+	public int fontSize = 50;
+	public Color fontColor = Color.BLACK;
+	public Color backGroundColor = Color.WHITE;
+
 	public Tokei() {
 		super();
-		setSize(200, 100);
+
+		setResizable(false);
 		setVisible(true);
-		
+
 		// Close
 		addWindowListener(new WindowAdapter() {
 			@Override
@@ -20,19 +33,21 @@ public class Tokei extends Frame implements Runnable {
 				System.exit(0);
 			}
 		});
-		
+
 		// Menu
 		MenuBar menuBar = new MenuBar();
-        setMenuBar(menuBar);
-        Menu menuFile = new Menu("File");
-        menuBar.add(menuFile);
-        menuFile.addActionListener(new ActionListener() {
+		setMenuBar(menuBar);
+		Menu menuFile = new Menu("File");
+		menuBar.add(menuFile);
+		MenuItem menuSettings = new MenuItem("Settings", new MenuShortcut('O'));
+		menuSettings.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Dialog dialog = new Dialog(Tokei.this);
-		        dialog.show();
+				Dialog dialog = new SettingDialog(Tokei.this);
+				dialog.setVisible(true);
 			}
 		});
+		menuFile.add(menuSettings);
 	}
 
 	public void run() {
@@ -46,11 +61,56 @@ public class Tokei extends Frame implements Runnable {
 	}
 
 	public void paint(Graphics g) {
+		// 時間文字列を算出
 		Calendar now = Calendar.getInstance();
 		int h = now.get(Calendar.HOUR_OF_DAY);
 		int m = now.get(Calendar.MINUTE);
 		int s = now.get(Calendar.SECOND);
-		g.drawString(String.format("%s:%s:%s", h, m, s), 50, 50);
+		String time = String.format("%02d:%02d:%02d", h, m, s);
+
+		// 文字列の大きさを測る
+		if (fontFamily != null)
+			g.setFont(new Font(fontFamily, Font.PLAIN, fontSize));
+		else
+			g.setFont(new Font("Serif", Font.PLAIN, fontSize));
+
+		FontMetrics ft = g.getFontMetrics();
+		int stringHeight = ft.getAscent();
+		int stringWidth = ft.stringWidth(time);
+		int standardWidth = ft.stringWidth("00:00:00");
+
+		// フレームの大きさを再設定
+		int height = stringHeight + MARGIN * 2;
+		int width = standardWidth + MARGIN * 2;
+		setSize(width, height);
+
+		// バッファーイメージを作成
+		if (imageBuffer == null || imageBuffer.getWidth(this) != width
+				|| imageBuffer.getHeight(this) != height) {
+			imageBuffer = createImage(width, height);
+			graphicsBuffer = null;
+		}
+
+		// バッファーグラフィックスを作成
+		if (graphicsBuffer == null) {
+			graphicsBuffer = imageBuffer.getGraphics();
+		}
+
+		// 背景を塗りつぶす
+		graphicsBuffer.setColor(backGroundColor);
+		graphicsBuffer.fillRect(0, 0, width, height);
+
+		// 時間文字列を描画
+		if (fontFamily != null)
+			graphicsBuffer.setFont(new Font(fontFamily, Font.PLAIN, fontSize));
+		else
+			graphicsBuffer.setFont(new Font("Serif", Font.PLAIN, fontSize));
+		graphicsBuffer.setColor(fontColor);
+		graphicsBuffer.drawString(time, MARGIN + (standardWidth - stringWidth)
+				/ 2, MARGIN + stringHeight);
+
+		// バッファを画面にコピーする
+		g.drawImage(imageBuffer, 0, 0, this);
 	}
 
 	public static void main(String args[]) {
