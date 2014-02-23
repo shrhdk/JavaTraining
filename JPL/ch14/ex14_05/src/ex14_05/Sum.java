88636d6a8872b822b29b27eccc23961a04a9b902
Hideki Shiro <hideki@shiro.be>
2014-02-23 17:02:07 +0900

    練習問題14.5を追加

---
+++ JPL/ch14/ex14_05/src/ex14_05/Sum.java
@@ -0,0 +1,10 @@
+package ex14_05;
+
+public class Sum {
+	private static int sum = 0;
+
+	public static int add(int value) {
+		sum += value;
+		return sum;
+	}
+}
---
+++ JPL/ch14/ex14_05/test/src/ex14_05/SumTest.java
@@ -0,0 +1,45 @@
+package ex14_05;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SumTest {
+	private static final int TIMES = 1000;
+	private static final int DIFF_A = 2;
+	private static final int DIFF_B = 3;
+
+	@Test
+	public void callAddFromMultiThread() {
+
+		Thread threadA = new Thread() {
+			@Override
+			public void run() {
+				for (int i = 0; i < TIMES; i++) {
+					synchronized (Sum.class) {
+						Sum.add(DIFF_A);
+					}
+				}
+			}
+		};
+		Thread threadB = new Thread() {
+			@Override
+			public void run() {
+				for (int i = 0; i < TIMES; i++) {
+					synchronized (Sum.class) {
+						Sum.add(DIFF_B);
+					}
+				}
+			}
+		};
+
+		threadA.start();
+		threadB.start();
+
+		while (threadA.isAlive() || threadB.isAlive())
+			;
+
+		assertThat(Sum.add(0), is((DIFF_A + DIFF_B) * TIMES));
+	}
+}
