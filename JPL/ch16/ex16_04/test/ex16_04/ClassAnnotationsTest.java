2be7e64837249138a2d4b91f4e13c1393142755a
Hideki Shiro <hideki@shiro.be>
2014-03-04 20:15:43 +0900

    16.4のテストを追加

--- JPL/ch16/ex16_04/test/ex16_04/ClassAnnotationsTest.java
+++ JPL/ch16/ex16_04/test/ex16_04/ClassAnnotationsTest.java
@@ -4,15 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 public class ClassAnnotationsTest {
 
 	@Test
 	public void test() {
-		ClassAnnotations.main(new String[] { "ex16_04.annotatedClass" });
+		ClassAnnotations.main(new String[] { "ex16_04.Annotated" });
 	}
 
 }
 
-class annotatedClass {
-	
+@Annotation1
+@Annotation2
+class Annotated {
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface Annotation1 {
+}
+
+@interface Annotation2 {
 }
