8036265ea60177fdf9717ef448c461f675a2f7ed
Hideki Shiro <hideki@shiro.be>
2014-04-09 13:30:29 +0900

    練習問題19.2を追加

---
+++ JPL/ch19/ex19_02/java/ex19_02/LinkedList.java
@@ -0,0 +1,84 @@
+package ex19_02;
+
+/**
+ * Item of linked list structure
+ */
+public class LinkedList {
+
+    /**
+     * The object of this linked list item
+     */
+	private Object value = null;
+
+    /**
+     * The next linked list item
+     */
+    private LinkedList next = null;
+
+    // Constructor
+
+    /**
+     * Construct linked list with specific objects;
+     * @param value Objects stored to new linked list
+     */
+	public LinkedList(Object value) {
+		this.value = value;
+	}
+
+    /**
+     *
+     * @param value
+     * @param next
+     */
+	public LinkedList(Object value, LinkedList next) {
+		this.value = value;
+		this.next = next;
+	}
+
+    // Setter and Getter
+
+    /**
+     * Get the value in this linked list item
+     * @param value The value to set
+     */
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    /**
+     * Get value in this linked list item
+     * @return The value of this linked list item
+     */
+    public Object getValue() {
+        return value;
+    }
+
+    /**
+     * Set the next linked list item
+     * @param next The next linked list item to set
+     */
+    public void setNext(LinkedList next) {
+        this.next = next;
+    }
+
+    /**
+     * Get the next linked list item
+     * @return The next linked list item
+     */
+    public LinkedList getNext() {
+        return next;
+    }
+
+    // Utility
+
+    /**
+     * Get length of this list
+     * @return Length of list
+     */
+    public int getLength() {
+        if (this.next == null)
+            return 1;
+        else
+            return 1 + this.next.getLength();
+    }
+}
